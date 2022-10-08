dcb59a0ee501146d42b7042ef5c183f4e9d63a70
parkwonyong <pwy6817@gmail.com>
2022-10-08 23:28:41 +0900

    todo-list add

---
+++ js/todo.js
@@ -0,0 +1,31 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function painToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "x";
+    button.addEventListener("click",deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    painToDo(newTodo);
+}
+
+
+toDoForm.addEventListener("submit", handleToDoSubmit) 
